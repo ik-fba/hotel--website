bf9d7d42a63b4917e383e649af6d8bafb78ff753
Ibrahim Kone <ikone10@mail.com>
2023-08-12 14:11:52 +0100

    commit

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// Get references to all "Add to Cart" buttons with the class "add-to-cart"
 const addToCartButtons = document.querySelectorAll('.add-to-cart');
 
-// Loop through each button and add a click event listener
 addToCartButtons.forEach(button => {
   button.addEventListener('click', function(event) {
     event.preventDefault();
     button.innerHTML = 'Added to Cart!';
     button.classList.add('added');
 
-    setTimeout(function() {
+    setTimeout(() => {
       button.innerHTML = 'Add to Cart <i class="bx bx-cart"></i>';
       button.classList.remove('added');
     }, 3000);
   });
 });
 
-// Your existing code for the menu toggle
 let menu = document.getElementById("menu");
 let navbar = document.querySelector(".navmenu");
 
